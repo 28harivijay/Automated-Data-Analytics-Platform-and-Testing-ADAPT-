a490ed41725c1fddd0e6a87c80917a0c4ec87f1b
28harivijay <28harivijay@gmail.com>
2026-03-26 14:42:43 -0400

    Added job recommendation system

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from ml_engine.recommender import recommend_jobs
+import pandas as pd
 
 def Home_Page(request):
     return render(request, 'home.html')
@@ -13,5 +14,27 @@ def Recommend(request):
 
 
 
+
 def Insights(request):
-    return render(request, 'insights.html')
+    df = pd.read_csv('datasets/job_market.csv')
+    df = df.dropna(subset=['skills', 'company', 'location'])
+    print(df['company'].value_counts().head(5))
+    print(df['location'].value_counts().head(5))
+
+    all_skills = df['skills'].str.split(', ').explode()
+    top_skills = all_skills.value_counts().head(10).to_dict()
+
+    top_companies = df['company'].value_counts().head(5).to_dict()
+
+    df['avg_salary'] = (df['salary_min'] + df['salary_max']) / 2
+    avg_salary = df.groupby('job_title')['avg_salary'].mean().round(0).sort_values(ascending=False).head(10).to_dict()
+
+    top_locations = df['location'].value_counts().head(5).to_dict()
+
+    context = {
+        'top_skills': top_skills,
+        'top_companies': top_companies,
+        'avg_salary': avg_salary,
+        'top_locations': top_locations,
+    }
+    return render(request, 'insights.html', context)
